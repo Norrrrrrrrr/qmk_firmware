26718560abb9cf8a9f92c2afed29235cf8f0f369
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-10-16 07:33:25 +0900

    commit in one rev

---
+++ keyboards/crkbd/keymaps/nor_dev/rules.mk
@@ -0,0 +1,19 @@
+OLED_DRIVER_ENABLE  = yes
+
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+CONSOLE_ENABLE = no         # Console for debug(+400)
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = no           # Audio output on port C6
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+SWAP_HANDS_ENABLE = no        # Enable one-hand typing
+
+TAP_DANCE_ENABLE = yes		# Enable Tap Dance
+
+IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
+#Link_Time_Optimization = no # if firmware size over limit, try this option
+#EXTRAFLAGS += -flto		# Link Time Optimization
+LTO_ENABLE = yes
