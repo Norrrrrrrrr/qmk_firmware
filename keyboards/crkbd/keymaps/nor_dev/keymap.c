7e99f6738ee5cd5057c5af99c7e1d498d816959d
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-10-17 22:36:13 +0900

    crkbd: update keymap like latest nirvana's one

--- keyboards/crkbd/keymaps/nor_dev/keymap.c
+++ keyboards/crkbd/keymaps/nor_dev/keymap.c
@@ -23,138 +23,183 @@ enum layer_number {
     _QWERTY = 0,
     _LOWER,
     _RAISE,
-    _NAVIGATION,
     _ADJUST
 };
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOW_SPC,
-  RAI_ENT,
-  NAVI,
-//   ADJUST,
+  KC_NOT_USED = SAFE_RANGE,
 };
 
 // Tap Dance declarations
 enum {
-    TD_LBRCS,
-    TD_RBRCS,
-    TD_BRCSET,
-    // TD_QUOT_APP,
-    TD_H_APP,
-    TD_MI_UD,
-    TD_EQ_PL,
-    // TD_E_ESC,
-    // TD_R_GRV,
-    // TD_Y_QUO,
-    // TD_U_MIN,
-    // TD_I_EQL,
-    // TD_O_BSL,
     TD_F1_F6,
     TD_F2_F7,
     TD_F3_F8,
     TD_F4_F9,
     TD_F5_F10,
-    TD_F11_F12,
+
+    TD_EX_11,
+    TD_AT_12,
+    TD_HS_ES,
+    TD_DL_GR,
+    TD_PR_TL,
+
+    TD_CR_AP,
+    TD_AM_L2,
+    TD_AS_R2,
+    TD_LB13,
+    TD_RB13,
+
+    TD_MI_UN,
+    TD_EQ_PL,
+    TD_QU_DQ,
+    TD_BL_PI,
+
+    TD_BRCSET
 };
 
-#define MT_A_SFT SFT_T(KC_A)
-#define MT_S_CTL CTL_T(KC_S)
-#define MT_D_GUI GUI_T(KC_D)
-#define MT_F_ALT ALT_T(KC_F)
-// #define MT_G_SFT SFT_T(KC_G)
+//Macros
+#define SF_A SFT_T(KC_A)
+#define CT_S CTL_T(KC_S)
+#define GU_D GUI_T(KC_D)
+#define AL_F ALT_T(KC_F)
 
-// #define MT_H_SFT SFT_T(KC_H)
-#define MT_J_ALT ALT_T(KC_J)
-#define MT_K_GUI GUI_T(KC_K)
-#define MT_L_CTL CTL_T(KC_L)
-#define MT_SC_SFT SFT_T(KC_SCLN)
+#define AL_J ALT_T(KC_J)
+#define GU_K GUI_T(KC_K)
+#define CT_L CTL_T(KC_L)
+#define SF_SC SFT_T(KC_SCLN)
 
 #define CG_LEFT C(G(KC_LEFT))
 #define CG_RGHT C(G(KC_RGHT))
 #define SG_LEFT S(G(KC_LEFT))
 #define SG_RGHT S(G(KC_RGHT))
 
-#define KC_LBRCS TD(TD_LBRCS)
-#define KC_RBRCS TD(TD_RBRCS)
+#define TT_F1F6 TD(TD_F1_F6)
+#define TT_F2F7 TD(TD_F2_F7)
+#define TT_F3F8 TD(TD_F3_F8)
+#define TT_F4F9 TD(TD_F4_F9)
+#define TT_F5F10 TD(TD_F5_F10)
+
+#define TT_EX_11 TD(TD_EX_11)
+#define TT_AT_12 TD(TD_AT_12)
+#define TT_HS_ES TD(TD_HS_ES)
+#define TT_DL_GR TD(TD_DL_GR)
+#define TT_PR_TL TD(TD_PR_TL)
+
+#define TT_CR_AP TD(TD_CR_AP)
+#define TT_AM_L2 TD(TD_AM_L2)
+#define TT_AS_R2 TD(TD_AS_R2)
+#define TT_LB13 TD(TD_LB13)
+#define TT_RB13 TD(TD_RB13)
+
+#define TT_MI_UN TD(TD_MI_UN)
+#define TT_EQ_PL TD(TD_EQ_PL)
+#define TT_QU_DQ TD(TD_QU_DQ)
+#define TT_BL_PI TD(TD_BL_PI)
+
+#define LOW_SPC LT(_LOWER, KC_SPC)
+#define RAI_ENT LT(_RAISE, KC_ENT)
+#define ADJ_SFT LT(_ADJUST, KC_LSFT)
+
 #define KC_BRCSET TD(TD_BRCSET)
-// #define KC_QTAPP TD(TD_QUOT_APP)
-#define KC_H_APP TD(TD_H_APP)
-#define KC_MI_UD TD(TD_MI_UD)
-#define KC_EQ_PL TD(TD_EQ_PL)
 
-#define KC_F1F6 TD(TD_F1_F6)
-#define KC_F2F7 TD(TD_F2_F7)
-#define KC_F3F8 TD(TD_F3_F8)
-#define KC_F4F9 TD(TD_F4_F9)
-#define KC_F5F10 TD(TD_F5_F10)
-#define KC_F1112 TD(TD_F11_F12)
-
-// #define LOW_SPC LT(_LOWER, KC_SPC)
-// #define RAI_ENT LT(_RAISE, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_GESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
+     KC_GESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,MT_A_SFT,MT_S_CTL,MT_D_GUI,MT_F_ALT,    KC_G,                     KC_H_APP,MT_J_ALT,MT_K_GUI,MT_L_CTL,MT_SC_SFT,KC_QUOT,
+      KC_TAB,   SF_A,    CT_S,    GU_D,    AL_F,    KC_G,                         KC_H,    AL_J,    GU_K,    CT_L,   SF_SC,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
+     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                             NAVI, KC_LSFT, LOW_SPC,    RAI_ENT, KC_BSPC,KC_BRCSET
+                                         XXXXXXX, LOW_SPC, ADJ_SFT,    KC_BSPC, RAI_ENT, KC_BRCSET
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_UNDS,
+      KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR,KC_LBRCS,KC_RBRCS, KC_BSLS,
+     _______, TT_EX_11,TT_AT_12,TT_HS_ES,TT_DL_GR,TT_PR_TL,                  TT_CR_AP,TT_AM_L2,TT_AS_R2,TT_LB13, TT_RB13, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_F1F6, KC_F2F7, KC_F3F8, KC_F4F9,KC_F5F10,                     KC_F1112,KC_MI_UD,KC_EQ_PL, _______, _______, KC_PLUS,
+      _______, TT_F1F6, TT_F2F7, TT_F3F8, TT_F4F9, TT_F5F10,                  TT_MI_UN,TT_EQ_PL,TT_QU_DQ,_______, TT_BL_PI,KC_PLUS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                         XXXXXXX, LOW_SPC, _______,   KC_BRCSET, KC_ENT,  XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      CG_LEFT, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, CG_RGHT,
+     KC_GESC, CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,                      XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX,                    C(KC_INS), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,S(KC_INS),
+     _______, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,                    KC_LANG1, KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      SG_LEFT, KC_BSPC,S(KC_TAB), KC_TAB,  KC_DEL, SG_RGHT,
+     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_BSPC,S(KC_TAB), KC_TAB,  KC_DEL, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [_NAVIGATION] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,                      _______, _______, _______, _______, _______, _______,
-
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,                      _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, KC_BTN1, KC_BTN3, KC_BTN2, _______,                      _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                         XXXXXXX,  KC_SPC, _______,    KC_BSPC, RAI_ENT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,   RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,  KC_INS,
+     XXXXXXX,  RESET,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,  KC_INS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                     KC_LANG2, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,KC_LANG1,
+     XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                         XXXXXXX,  KC_SPC, _______,    KC_BSPC,  KC_ENT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LOW_SPC:
+        case RAI_ENT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    /* Aoid to mistake mod */
+    case GU_D:
+    case GU_K:
+      return 200;
+    /* Easier for Tap Dance */
+    case TT_F1F6:
+    case TT_F2F7:
+    case TT_F3F8:
+    case TT_F4F9:
+    case TT_F5F10:
+
+    case TT_EX_11:
+    case TT_AT_12:
+    case TT_HS_ES:
+    case TT_DL_GR:
+    case TT_PR_TL:
+
+    case TT_CR_AP:
+    case TT_AM_L2:
+    case TT_AS_R2:
+    case TT_LB13:
+    case TT_RB13:
+
+    case TT_MI_UN:
+    case TT_EQ_PL:
+    case TT_QU_DQ:
+    case TT_BL_PI:
+      return 200;
+    case KC_BRCSET:
+      return 300;
+    default:
+      return TAPPING_TERM;
+  }
+}
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -167,8 +212,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 #define L_BASE 0
 #define L_LOWER 2
 #define L_RAISE 4
-// #define L_ADJUST 8
-#define L_ADJUST 16
+#define L_ADJUST 8
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
@@ -254,106 +298,15 @@ void oled_task_user(void) {
 }
 #endif // OLED_DRIVER_ENABLE
 
-// Setting ADJUST
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
-static bool is_lower_pressed = false;
-static uint16_t lower_pressed_time = 0;
-static bool is_raise_pressed = false;
-static uint16_t raise_pressed_time = 0;
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
   #ifdef OLED_DRIVER_ENABLE
     set_keylog(keycode, record);
   #endif // OLED_DRIVER_ENABLE
   }
-  switch (keycode) {
-    case LOW_SPC:
-      if (record->event.pressed) {
-        is_lower_pressed = true;
-        lower_pressed_time = record->event.time;
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-        if (is_lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
-          tap_code(KC_SPC);
-        }
-        is_lower_pressed = false;
-      }
-      return false;
-      break;
-
-    case RAI_ENT:
-      if (record->event.pressed) {
-        is_raise_pressed = true;
-        raise_pressed_time = record->event.time;
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-        if (is_raise_pressed && (TIMER_DIFF_16(record->event.time, raise_pressed_time) < TAPPING_TERM)) {
-          tap_code(KC_ENT);
-        }
-        is_raise_pressed = false;
-      }
-      return false;
-      break;
-    
-    case NAVI:
-      if (record->event.pressed) {
-        layer_on(_NAVIGATION);
-      } else {
-        layer_off(_NAVIGATION);
-      }
-      return false;
-      break;
-
-    default:
-      if (record->event.pressed) {
-        is_lower_pressed = false;
-        is_raise_pressed = false;
-      }
-      break;
-  }
   return true;
 }
 
-
-void dance_lbrackets(qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    SEND_STRING("(");
-  }
-  else if (state->count == 2) {
-    SEND_STRING("[");
-  }
-  else if (state->count >= 3) {
-    SEND_STRING("{");
-    reset_tap_dance(state);
-  }
-}
-
-void dance_rbrackets(qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    SEND_STRING(")");
-  }
-  else if (state->count == 2) {
-    SEND_STRING("]");
-  }
-  else if (state->count >= 3) {
-    SEND_STRING("}");
-    reset_tap_dance(state);
-  }
-}
 
 void dance_brackets_set(qk_tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
@@ -370,23 +323,28 @@ void dance_brackets_set(qk_tap_dance_state_t *state, void *user_data) {
 
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LBRCS] = ACTION_TAP_DANCE_FN(dance_lbrackets),
-    [TD_RBRCS] = ACTION_TAP_DANCE_FN(dance_rbrackets),
-    [TD_BRCSET] = ACTION_TAP_DANCE_FN(dance_brackets_set),
-    // [TD_QUOT_APP] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_APP),
-    [TD_H_APP] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_APP),
-    [TD_MI_UD] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, S(KC_MINS)),
-    [TD_EQ_PL] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, S(KC_EQL)),
-    // [TD_E_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_E, KC_ESC),
-    // [TD_R_GRV] = ACTION_TAP_DANCE_DOUBLE(KC_R, KC_GRV),
-    // [TD_Y_QUO] = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_QUOT),
-    // [TD_U_MIN] = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_MINS),
-    // [TD_I_EQL] = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_EQL),
-    // [TD_O_BSL] = ACTION_TAP_DANCE_DOUBLE(KC_O, KC_BSLS),
     [TD_F1_F6] = ACTION_TAP_DANCE_DOUBLE(KC_F1, KC_F6),
     [TD_F2_F7] = ACTION_TAP_DANCE_DOUBLE(KC_F2, KC_F7),
     [TD_F3_F8] = ACTION_TAP_DANCE_DOUBLE(KC_F3, KC_F8),
     [TD_F4_F9] = ACTION_TAP_DANCE_DOUBLE(KC_F4, KC_F9),
     [TD_F5_F10] = ACTION_TAP_DANCE_DOUBLE(KC_F5, KC_F10),
-    [TD_F11_F12] = ACTION_TAP_DANCE_DOUBLE(KC_F11, KC_F12),
+
+    [TD_EX_11] = ACTION_TAP_DANCE_DOUBLE(KC_EXLM, KC_F11),
+    [TD_AT_12] = ACTION_TAP_DANCE_DOUBLE(KC_AT, KC_F12),
+    [TD_HS_ES] = ACTION_TAP_DANCE_DOUBLE(KC_HASH, KC_ESC),
+    [TD_DL_GR] = ACTION_TAP_DANCE_DOUBLE(KC_DLR, KC_GRV),
+    [TD_PR_TL] = ACTION_TAP_DANCE_DOUBLE(KC_PERC, KC_TILD),
+
+    [TD_CR_AP] = ACTION_TAP_DANCE_DOUBLE(KC_CIRC, KC_APP),
+    [TD_AM_L2] = ACTION_TAP_DANCE_DOUBLE(KC_AMPR, KC_LBRC),
+    [TD_AS_R2] = ACTION_TAP_DANCE_DOUBLE(KC_ASTR, KC_RBRC),
+    [TD_LB13] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_LCBR),
+    [TD_RB13] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN, KC_RCBR),
+
+    [TD_MI_UN] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_UNDS),
+    [TD_EQ_PL] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_PLUS),
+    [TD_QU_DQ] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
+    [TD_BL_PI] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_PIPE),
+
+    [TD_BRCSET] = ACTION_TAP_DANCE_FN(dance_brackets_set)
 };
