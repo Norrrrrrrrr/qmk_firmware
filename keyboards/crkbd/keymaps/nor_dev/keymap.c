cc6f6c8c075ce6a58760f5fbe4cd09571ca539a0
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-10-23 21:56:30 +0900

    Change an include file to reduce "problems"

--- keyboards/crkbd/keymaps/nor_dev/keymap.c
+++ keyboards/crkbd/keymaps/nor_dev/keymap.c
@@ -17,7 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include "keycode.h"
+// #include "keycode.h"
+#include "quantum_keycodes.h"
 
 enum layer_number {
     _QWERTY = 0,
