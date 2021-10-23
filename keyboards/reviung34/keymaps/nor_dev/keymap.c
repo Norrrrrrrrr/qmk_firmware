6fe87e2b4a6ea5fce0178b4e13aebbf4f5910a99
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-10-23 21:56:30 +0900

    Change an include file to reduce "problems"

--- keyboards/reviung34/keymaps/nor_dev/keymap.c
+++ keyboards/reviung34/keymaps/nor_dev/keymap.c
@@ -14,7 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "keycode.h"
+// #include "keycode.h"
+#include "quantum_keycodes.h"
 
 enum layer_names {
   _BASE,
