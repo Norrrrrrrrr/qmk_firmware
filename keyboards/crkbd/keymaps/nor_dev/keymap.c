24e0b627ac106edca5d776cac2a8699d74698c8e
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-11-07 19:35:10 +0900

    crkbd: disable LTO, remove one KC_BRCSET

--- keyboards/crkbd/keymaps/nor_dev/keymap.c
+++ keyboards/crkbd/keymaps/nor_dev/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         XXXXXXX, LOW_SPC, ADJ_SFT,    KC_BSPC, RAI_ENT, KC_BRCSET
+                                         XXXXXXX, LOW_SPC, ADJ_SFT,    KC_BSPC, RAI_ENT, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
