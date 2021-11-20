318e8770ea409c8476cc4b6523460831688d85cb
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-11-20 11:09:37 +0900

    nirvana: add F_KEY layer, include keycode.h

--- keyboards/nirvana_nn/keymaps/default/keymap.c
+++ keyboards/nirvana_nn/keymaps/default/keymap.c
@@ -14,13 +14,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-// #include "keycode.h"
+#include "keycode.h"
 #include "quantum_keycodes.h"
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY = 0,
     _LOWER,
+    _F_KEY,
     _RAISE,
     _ADJUST
 };
@@ -99,7 +100,8 @@ enum {
 
 #define LOW_SPC LT(_LOWER, KC_SPC)
 #define RAI_ENT LT(_RAISE, KC_ENT)
-#define ADJ_SFT LT(_ADJUST, KC_LSFT)
+#define FKY_SFT LT(_F_KEY, KC_LSFT)
+#define ADJ_BS LT(_ADJUST, KC_BSPC)
 
 #define KC_BRCSET TD(TD_BRCSET)
 
@@ -113,33 +115,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 | Shift|   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  |   ,  |   .  |   /  |   =  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-|      |      |      |      |LowSpc|AdjSft|  |  BS  |RaiEnt|      |      |      |      |
+|      |      |      |      |LowSpc|FkySft|  | AdjBS|RaiEnt|      |      |      |      |
 `-----------------------------------------'  `-----------------------------------------'
    */
   [_QWERTY] = LAYOUT(
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       KC_TAB,  SF_A,    CT_S,    GU_D,    AL_F,     KC_G,                     KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,   KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOW_SPC,  ADJ_SFT,                  KC_BSPC, RAI_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOW_SPC,  FKY_SFT,                  ADJ_BS,  RAI_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
       ),
 
   /* Lower
 ,-----------------------------------------.  ,-----------------------------------------.
 |   `  |   1  |   2  |   3  |   4  |   5  |  |   6  |   7  |   8  |   9  |   0  |   _  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-| Tab  | ! F11| @ F12| # ESC| $  ` | %  ~ |  | ^ App| &  [ | *  ] | (  { | )  } |   \  |
+| Tab  |   !  |   @  |   #  |   $  |   %  |  | ^ App|   &  |   *  |   (  |   )  |   \  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-| Shift| F1 F6| F2 F7| F3 F8| F4 F9|F5 F10|  | -  _ | =  + | '  " |   .  | \  | |   +  |
+| Shift|      |      |      |      |      |  |   -  |   =  |   '  |   .  |   \  |   +  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-|      |      |      |      |LowSpc|AdjSft|  |()[]{}| Enter|      |      |      |      |
+|      |      |      |      |LowSpc|      |  BS  | Enter|      |      |      |      |
 `-----------------------------------------'  `-----------------------------------------'
-
    */
   [_LOWER] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS, \
-      _______, TT_EX_11,TT_AT_12,TT_HS_ES,TT_DL_GR,TT_PR_TL,                  TT_CR_AP,TT_AM_L2,TT_AS_R2,TT_LB13, TT_RB13, KC_BSLS, \
-      _______, TT_F1F6, TT_F2F7, TT_F3F8, TT_F4F9, TT_F5F10,                  TT_MI_UN,TT_EQ_PL,TT_QU_DQ,_______, TT_BL_PI,KC_PLUS, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOW_SPC, _______,                  KC_BRCSET,KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_EQL,  KC_QUOT, _______, KC_BSLS, KC_PLUS, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOW_SPC, XXXXXXX,                   KC_BSPC, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+      ),
+
+  /* F_Key
+,-----------------------------------------.  ,-----------------------------------------.
+|   `  |   F1 |   F2 |   F3 |   F4 |   F5 |  |   F6 |   F7 |   F8 |   F9 |  F10 |   _  |
+|------+------+------+------+------+------|  |------+------+------+------+------+------|
+| Tab  |  F11 |  F12 |  ESC |   `  |   ~  |  |  App |   [  |   ]  |   {  |   }  |   \  |
+|------+------+------+------+------+------|  |------+------+------+------+------+------|
+| Shift|      |      |      |      |      |  |   _  |   +  |   "  |   .  |   |  |   +  |
+|------+------+------+------+------+------|  |------+------+------+------+------+------|
+|      |      |      |      | Space|FkySft|  |FKeyBs| Enter|      |      |      |      |
+`-----------------------------------------'  `-----------------------------------------'
+   */
+  [_F_KEY] = LAYOUT( \
+      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_UNDS, \
+      _______, KC_F11,  KC_F12,  KC_ESC,  KC_GRV,  KC_TILD,                   KC_APP,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_BSLS, \
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_UNDS, KC_PLUS, KC_DQT,  KC_DOT,  KC_PIPE, KC_PLUS, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  FKY_SFT,                   KC_BSPC, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
       ),
 
   /* Raise
@@ -150,14 +169,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 | Shift|      |      |      |      |      |  |      |  BS  |SftTab| Tab  | Del  |      |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-|      |      |      |      | Space|AdjSft|  |  BS  |RaiEnt|      |      |      |      |
+|      |      |      |      | Space|      |  |  BS  |RaiEnt|      |      |      |      |
 `-----------------------------------------'  `-----------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       KC_GESC, CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,                   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, \
       _______, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,                  KC_LANG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,  XXXXXXX, \
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  _______,                   KC_BSPC, RAI_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX,                   KC_BSPC, RAI_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
       ),
 
   /* Adjust
@@ -168,14 +187,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 |      |      |      |      |      |      |  |      |      |      |      |      |      |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-|      |      |      |      | Space|AdjSft|  |  BS  | Enter|      |      |      |      |
+|      |      |      |      | Space|      |  | AdjBS| Enter|      |      |      |      |
 `-----------------------------------------'  `-----------------------------------------'
    */
     [_ADJUST] =  LAYOUT( \
       XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,  XXXXXXX, \
       XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX,\
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  _______,                   KC_BSPC, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX,                   ADJ_BS,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX\
       )
 };
 
