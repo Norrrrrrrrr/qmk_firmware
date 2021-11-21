482cadca8ec4bd0c3b7cb2798b0ae4fa31ccce35
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-11-21 14:49:57 +0900

    reviung: update keymaps without tap dance

--- keyboards/reviung34/keymaps/nor_dev/rules.mk
+++ keyboards/reviung34/keymaps/nor_dev/rules.mk
@@ -5,6 +5,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # LTO_ENABLE = yes
-TAP_DANCE_ENABLE = yes		# Enable Tap Dance
+# TAP_DANCE_ENABLE = yes		# Enable Tap Dance
 #e nn_rev01
 
