dd540b4e75feab1f9d13695e41d8d264d2ed8f68
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-11-21 14:49:57 +0900

    reviung: update keymaps without tap dance

--- keyboards/reviung34/keymaps/nor_dev/keymap.c
+++ keyboards/reviung34/keymaps/nor_dev/keymap.c
@@ -14,42 +14,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-// #include "keycode.h"
+#include "keycode.h"
 #include "quantum_keycodes.h"
 
 enum layer_names {
-  _BASE,
+  _BASE = 0,
   _LOWER,
+  _F_KEY,
   _RAISE,
   _ADJUST
-};
-
-// Tap Dance declarations
-enum {
-    TD_F1_F6,
-    TD_F2_F7,
-    TD_F3_F8,
-    TD_F4_F9,
-    TD_F5_F10,
-
-    TD_EX_11,
-    TD_AT_12,
-    TD_HS_ES,
-    TD_DL_GR,
-    TD_PR_TL,
-
-    TD_CR_AP,
-    TD_AM_L2,
-    TD_AS_R2,
-    TD_LB13,
-    TD_RB13,
-
-    TD_MI_UN,
-    TD_EQ_PL,
-    TD_QU_DQ,
-    TD_BL_PI,
-
-    TD_BRCSET,
 };
 
 #define SF_A SFT_T(KC_A)
@@ -67,34 +40,10 @@ enum {
 #define SG_LEFT S(G(KC_LEFT))
 #define SG_RGHT S(G(KC_RGHT))
 
-#define TT_F1F6 TD(TD_F1_F6)
-#define TT_F2F7 TD(TD_F2_F7)
-#define TT_F3F8 TD(TD_F3_F8)
-#define TT_F4F9 TD(TD_F4_F9)
-#define TT_F5F10 TD(TD_F5_F10)
-
-#define TT_EX_11 TD(TD_EX_11)
-#define TT_AT_12 TD(TD_AT_12)
-#define TT_HS_ES TD(TD_HS_ES)
-#define TT_DL_GR TD(TD_DL_GR)
-#define TT_PR_TL TD(TD_PR_TL)
-
-#define TT_CR_AP TD(TD_CR_AP)
-#define TT_AM_L2 TD(TD_AM_L2)
-#define TT_AS_R2 TD(TD_AS_R2)
-#define TT_LB13 TD(TD_LB13)
-#define TT_RB13 TD(TD_RB13)
-
-#define TT_MI_UN TD(TD_MI_UN)
-#define TT_EQ_PL TD(TD_EQ_PL)
-#define TT_QU_DQ TD(TD_QU_DQ)
-#define TT_BL_PI TD(TD_BL_PI)
-
 #define LOW_SPC LT(_LOWER, KC_SPC)
 #define RAI_ENT LT(_RAISE, KC_ENT)
-#define ADJ_SFT LT(_ADJUST, KC_LSFT)
-
-#define KC_BRCSET TD(TD_BRCSET)
+#define FKY_SFT LT(_F_KEY, KC_LSFT)
+#define ADJ_BS LT(_ADJUST, KC_BSPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -106,32 +55,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  |   ,  |   .  |   /  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     |LowSpc|AdjSft|  |  BS  |RaiEnt|
+                     |LowSpc|FkySft|  | AdjBS|RaiEnt|
                      `-------------'  `-------------'
    */
   [_BASE] = LAYOUT_reviung34(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     SF_A,    CT_S,    GU_D,    AL_F,    KC_G,          KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                               LOW_SPC, ADJ_SFT,       KC_BSPC, RAI_ENT
+                               LOW_SPC, FKY_SFT,       ADJ_BS, RAI_ENT
   ),
 
   /* LOWER
 ,----------------------------------.  ,----------------------------------.
 |   1  |   2  |   3  |   4  |   5  |  |   6  |   7  |   8  |   9  |   0  |
 +------+------+------+------+------|  |------+------+------+------+------|
-| ! F11| @ F12| # ESC| $  ` | %  ~ |  | ^ App| &  [ | *  ] | (  { | )  } |
+|   !  |   @  |   #  |   $  |   %  |  |   ^  |   &  |   *  |   (  |   )  |
 +------+------+------+------+------|  |------+------+------+------+------|
-| F1 F6| F2 F7| F3 F8| F4 F9|F5 F10|  | -  _ | =  + | '  " |   .  | \  | |
+|      |      |      |      |      |  |   [  |   ]  |   {  |   .  |   }  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     |LowSpc|AdjSft|  |()[]{}| Enter|
+                     |LowSpc|      |  |  BS  | Enter|
                      `-------------'  `-------------'
    */
   [_LOWER] = LAYOUT_reviung34(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    TT_EX_11,TT_AT_12,TT_HS_ES,TT_DL_GR,TT_PR_TL,      TT_CR_AP,TT_AM_L2,TT_AS_R2,TT_LB13, TT_RB13,
-    TT_F1F6, TT_F2F7, TT_F3F8, TT_F4F9, TT_F5F10,      TT_MI_UN,TT_EQ_PL,TT_QU_DQ,_______, TT_BL_PI,
-                               LOW_SPC, _______,      KC_BRCSET,KC_ENT
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_LBRC, KC_RBRC, KC_LCBR, KC_DOT,  KC_RCBR,
+                               LOW_SPC, XXXXXXX,       KC_BSPC, KC_ENT
+  ),
+
+  /* F_Key
+,----------------------------------.  ,----------------------------------.
+|   F1 |   F2 |   F3 |   F4 |   F5 |  |   F6 |   F7 |   F8 |   F9 |  F10 |
+|------+------+------+------+------|  |------+------+------+------+------|
+|  F11 |  F12 |  ESC |   `  |   ~  |  |   _  |   +  |   "  |  App |   |  |
+|------+------+------+------+------|  |------+------+------+------+------|
+|      |      |      |      |      |  |   -  |   =  |   '  |      |   \  |
+`------+------+------+------+------|  |------+------+------+------+------'
+                     | Space|FkySft|  |  BS  | Enter|
+                     `-------------'  `-------------'
+   */
+  [_F_KEY] = LAYOUT_reviung34(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_F11,  KC_F12,  KC_ESC,  KC_GRV,  KC_TILD,       KC_UNDS, KC_PLUS, KC_DQT,  KC_APP,  KC_PIPE,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_MINS, KC_EQL,  KC_QUOT, XXXXXXX, KC_BSLS,
+                               KC_SPC,  FKY_SFT,       KC_BSPC, KC_ENT
   ),
 
   /* RAISE
@@ -142,14 +109,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |      |      |      |      |      |  |      |  BS  |SftTab| Tab  | Del  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     | Space|AdjSft|  |  BS  |RaiEnt|
+                     | Space|      |  |  BS  |RaiEnt|
                      `-------------'  `-------------'
    */
   [_RAISE] = LAYOUT_reviung34(
     CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,     KC_LANG1, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,      KC_LANG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
-                               KC_SPC,  _______,       KC_BSPC, RAI_ENT
+                               KC_SPC,  XXXXXXX,       KC_BSPC, RAI_ENT
   ),
 
   /* ADJUST
@@ -160,20 +127,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |RGB ON| VAL- | SAT- | HUE- | MODE-|  |      |      |      |      |      |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     | Space|AdjSft|  |  BS  | Enter|
+                     | Space|      |  | AdjBS| Enter|
                      `-------------'  `-------------'
    */
   [_ADJUST] = LAYOUT_reviung34(
     RESET,   RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
     XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,
     RGB_TOG, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                               KC_SPC,  _______,       _______, KC_ENT
+                               KC_SPC,  _______,       ADJ_BS, KC_ENT
   ),
 };
-
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-// }
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -188,61 +151,19 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    /* Typing mod and layer quickly */
-    case SF_A:
-    case CT_S:
-    case AL_F:
-    case AL_J:
-    case CT_L:
-    case SF_SC:
-        return 160;
+    /* Avoid mis-type */
+    case GU_D:
+    case GU_K:
+        return 200;
+    case ADJ_BS:
+        return 500;
+    /* Typing layer quickly */
     case LOW_SPC:
     case RAI_ENT:
+    case FKY_SFT:
         return 130;
-    /* Easier for Tap Dance */
-    case KC_BRCSET:
-      return 300;
     default:
-      return 200;
+      return 160;
   }
 }
 
-void dance_brackets_set(qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    SEND_STRING("()"SS_TAP(X_LEFT));
-  }
-  else if (state->count == 2) {
-    SEND_STRING("[]"SS_TAP(X_LEFT));
-  }
-  else if (state->count >= 3) {
-    SEND_STRING("{}"SS_TAP(X_LEFT));
-    reset_tap_dance(state);
-  }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_F1_F6] = ACTION_TAP_DANCE_DOUBLE(KC_F1, KC_F6),
-    [TD_F2_F7] = ACTION_TAP_DANCE_DOUBLE(KC_F2, KC_F7),
-    [TD_F3_F8] = ACTION_TAP_DANCE_DOUBLE(KC_F3, KC_F8),
-    [TD_F4_F9] = ACTION_TAP_DANCE_DOUBLE(KC_F4, KC_F9),
-    [TD_F5_F10] = ACTION_TAP_DANCE_DOUBLE(KC_F5, KC_F10),
-
-    [TD_EX_11] = ACTION_TAP_DANCE_DOUBLE(KC_EXLM, KC_F11),
-    [TD_AT_12] = ACTION_TAP_DANCE_DOUBLE(KC_AT, KC_F12),
-    [TD_HS_ES] = ACTION_TAP_DANCE_DOUBLE(KC_HASH, KC_ESC),
-    [TD_DL_GR] = ACTION_TAP_DANCE_DOUBLE(KC_DLR, KC_GRV),
-    [TD_PR_TL] = ACTION_TAP_DANCE_DOUBLE(KC_PERC, KC_TILD),
-
-    [TD_CR_AP] = ACTION_TAP_DANCE_DOUBLE(KC_CIRC, KC_APP),
-    [TD_AM_L2] = ACTION_TAP_DANCE_DOUBLE(KC_AMPR, KC_LBRC),
-    [TD_AS_R2] = ACTION_TAP_DANCE_DOUBLE(KC_ASTR, KC_RBRC),
-    [TD_LB13] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_LCBR),
-    [TD_RB13] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN, KC_RCBR),
-
-    [TD_MI_UN] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_UNDS),
-    [TD_EQ_PL] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_PLUS),
-    [TD_QU_DQ] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_DQUO),
-    [TD_BL_PI] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_PIPE),
-
-    [TD_BRCSET] = ACTION_TAP_DANCE_FN(dance_brackets_set),
-};
