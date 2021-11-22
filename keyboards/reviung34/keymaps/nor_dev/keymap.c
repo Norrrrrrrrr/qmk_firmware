b5f5b00cedd50361b9196227735bff142342ada8
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-11-22 19:53:17 +0900

    reviung: change way to access Adjust layer

--- keyboards/reviung34/keymaps/nor_dev/keymap.c
+++ keyboards/reviung34/keymaps/nor_dev/keymap.c
@@ -20,7 +20,7 @@
 enum layer_names {
   _BASE = 0,
   _LOWER,
-  _F_KEY,
+  _LOWER2,
   _RAISE,
   _ADJUST
 };
@@ -42,8 +42,8 @@ enum layer_names {
 
 #define LOW_SPC LT(_LOWER, KC_SPC)
 #define RAI_ENT LT(_RAISE, KC_ENT)
-#define FKY_SFT LT(_F_KEY, KC_LSFT)
-#define ADJ_BS LT(_ADJUST, KC_BSPC)
+#define LO2_SFT LT(_LOWER2, KC_LSFT)
+#define KC_ADJ MO(_ADJUST)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  |   ,  |   .  |   /  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     |LowSpc|FkySft|  | AdjBS|RaiEnt|
+                     |LowSpc|Lo2Sft|  |  BS  |RaiEnt|
                      `-------------'  `-------------'
    */
   [_BASE] = LAYOUT_reviung34(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     SF_A,    CT_S,    GU_D,    AL_F,    KC_G,          KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                               LOW_SPC, FKY_SFT,       ADJ_BS, RAI_ENT
+                               LOW_SPC, LO2_SFT,       KC_BSPC, RAI_ENT
   ),
 
   /* LOWER
@@ -73,17 +73,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 +------+------+------+------+------|  |------+------+------+------+------|
 |      |      |      |      |      |  |   [  |   ]  |   {  |   .  |   }  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     |LowSpc|      |  |  BS  | Enter|
+                     |LowSpc|Adjust|  |  BS  | Enter|
                      `-------------'  `-------------'
    */
   [_LOWER] = LAYOUT_reviung34(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_LBRC, KC_RBRC, KC_LCBR, KC_DOT,  KC_RCBR,
-                               LOW_SPC, XXXXXXX,       KC_BSPC, KC_ENT
+                               LOW_SPC, KC_ADJ,        _______, KC_ENT
   ),
 
-  /* F_Key
+  /* LOWER2
 ,----------------------------------.  ,----------------------------------.
 |   F1 |   F2 |   F3 |   F4 |   F5 |  |   F6 |   F7 |   F8 |   F9 |  F10 |
 |------+------+------+------+------|  |------+------+------+------+------|
@@ -91,14 +91,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |      |      |      |      |      |  |   -  |   =  |   '  |      |   \  |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     | Space|FkySft|  |  BS  | Enter|
+                     |Adjust|Lo2Sft|  |  BS  | Enter|
                      `-------------'  `-------------'
    */
-  [_F_KEY] = LAYOUT_reviung34(
+  [_LOWER2] = LAYOUT_reviung34(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_F11,  KC_F12,  KC_ESC,  KC_GRV,  KC_TILD,       KC_UNDS, KC_PLUS, KC_DQT,  KC_APP,  KC_PIPE,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_MINS, KC_EQL,  KC_QUOT, XXXXXXX, KC_BSLS,
-                               KC_SPC,  FKY_SFT,       KC_BSPC, KC_ENT
+                               KC_ADJ,  LO2_SFT,       _______, KC_ENT
   ),
 
   /* RAISE
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,      KC_LANG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
-                               KC_SPC,  XXXXXXX,       KC_BSPC, RAI_ENT
+                               KC_SPC,  XXXXXXX,       _______, RAI_ENT
   ),
 
   /* ADJUST
@@ -127,14 +127,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |RGB ON| VAL- | SAT- | HUE- | MODE-|  |      |      |      |      |      |
 `------+------+------+------+------|  |------+------+------+------+------'
-                     | Space|      |  | AdjBS| Enter|
+                     |LowSpc|Lo2Sft|  |  BS  | Enter|
                      `-------------'  `-------------'
    */
   [_ADJUST] = LAYOUT_reviung34(
     RESET,   RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
     XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,
     RGB_TOG, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                               KC_SPC,  _______,       ADJ_BS, KC_ENT
+                               _______,  _______,      _______, KC_ENT
   ),
 };
 
@@ -142,6 +142,7 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LOW_SPC:
         case RAI_ENT:
+        case LO2_SFT:
             return true;
             break;
         default:
@@ -155,12 +156,10 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case GU_D:
     case GU_K:
         return 200;
-    case ADJ_BS:
-        return 500;
     /* Typing layer quickly */
     case LOW_SPC:
     case RAI_ENT:
-    case FKY_SFT:
+    case LO2_SFT:
         return 130;
     default:
       return 160;
