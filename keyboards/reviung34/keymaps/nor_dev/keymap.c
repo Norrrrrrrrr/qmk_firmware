1b8e08bbb262c9670b1a16962d91f0c780b50073
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-12-15 21:52:13 +0900

    Revert "reviung:Add key for mouse wheel"
    This reverts commit 421b7c033ca207a29dc70ca32bac326430901b09.

--- keyboards/reviung34/keymaps/nor_dev/keymap.c
+++ keyboards/reviung34/keymaps/nor_dev/keymap.c
@@ -1,4 +1,3 @@
-
 /* Copyright 2021 gtips
  *
  * This program is free software: you can redistribute it and/or modify
@@ -90,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |  F1  |  F2  |  F3  |  F4  |  F5  |  |  F6  |  F7  |  F8  |  F9  |  F10 |
 |------+------+------+------+------|  |------+------+------+------+------|
-|  F11 |  F12 | Wh_L | Wh_R |      |  |      |      |   ,  |   .  |      |
+|  F11 |  F12 |      |      |      |  |      |      |   ,  |   .  |      |
 `------+------+------+------+------|  |------+------+------+------+------'
                      |Adjust|Lo2Sft|  |  BS  | Enter|
                      `-------------'  `-------------'
@@ -98,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER2] = LAYOUT_reviung34(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_F11,  KC_F12,  KC_WH_L, KC_WH_R, XXXXXXX,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX,
+    KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX,
                                KC_ADJ,  LO2_SFT,       _______, KC_ENT
   ),
 
@@ -108,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 | Shift| Ctrl |  Gui |  Alt |ImeOff|  |ImeOn | Left | Down | Up   | Right|
 |------+------+------+------+------|  |------+------+------+------+------|
-| Wh_L | Wh_D | Wh_U | Wh_R |      |  |  App |  BS  |SftTab| Tab  | Del  |
+|      |      |      |      |      |  |  App |  BS  |SftTab| Tab  | Del  |
 `------+------+------+------+------|  |------+------+------+------+------'
                      | Space|      |  |  BS  |RaiEnt|
                      `-------------'  `-------------'
@@ -116,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_reviung34(
     CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,      KC_LANG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,       KC_APP,  KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_APP,  KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
                                KC_SPC,  XXXXXXX,       _______, RAI_ENT
   ),
 
