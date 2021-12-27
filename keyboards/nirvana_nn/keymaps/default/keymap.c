a7d9231f9e5094d1d6c80e2875390b1aa0d8fb6b
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-12-27 21:40:38 +0900

    nirvana: move Mouse Button to Low2

--- keyboards/nirvana_nn/keymaps/default/keymap.c
+++ keyboards/nirvana_nn/keymaps/default/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 | Tab  |  F1  |  F2  |  F3  |  F4  |  F5  |  |  F6  |  F7  |  F8  |  F9  |  F10 |   \  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-| Shift|  F11 |  F12 | Wh_L | Wh_R |      |  |      |      |   ,  |   .  |      |   +  |
+| Shift|  F11 |  F12 | BtnR | BtnM | BtnL |  |      |      |   ,  |   .  |      |   +  |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 |      |      |      |      |Adjust|Lo2Sft|  |  BS  | Enter|      |      |      |      |
 `-----------------------------------------'  `-----------------------------------------'
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER2] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSLS,
-      _______, KC_F11,  KC_F12,  KC_WH_L, KC_WH_R, XXXXXXX,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX, KC_PLUS,
+      _______, KC_F11,  KC_F12,  KC_BTN2, KC_BTN3, KC_BTN1,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX, KC_PLUS,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ADJ,  LO2_SFT,       _______, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
       ),
 
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,-----------------------------------------.  ,-----------------------------------------.
 |      | Reset|      |      |      |      |  |      |      |      |Print | Ins  |      |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
-|      |      | BtnR | BtnM | BtnL |      |  |      |      |      |ScLock|Pause |      |
+|      |      |      |      |      |      |  |      |      |      |ScLock|Pause |      |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
 |      |      |      |      |      |      |  |      |      |      |      |      |      |
 |------+------+------+------+------+------|  |------+------+------+------+------+------|
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_ADJUST] =  LAYOUT(
       XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,  XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,       _______, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
       )
