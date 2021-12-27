2613ddc104b8113aa0874823a5b401f017c26725
Norrrrrrrrr <nor.fenstrat@gmail.com>
2021-12-27 21:22:34 +0900

    reviung: move Mouse Button to Low2

--- keyboards/reviung34/keymaps/nor_dev/keymap.c
+++ keyboards/reviung34/keymaps/nor_dev/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|  |------+------+------+------+------|
 |  F1  |  F2  |  F3  |  F4  |  F5  |  |  F6  |  F7  |  F8  |  F9  |  F10 |
 |------+------+------+------+------|  |------+------+------+------+------|
-|  F11 |  F12 | Wh_L | Wh_R |      |  |      |      |   ,  |   .  |      |
+|  F11 |  F12 | BtnR | BtnM | BtnL |  |      |      |   ,  |   .  |      |
 `------+------+------+------+------|  |------+------+------+------+------'
                      |Adjust|Lo2Sft|  |  BS  | Enter|
                      `-------------'  `-------------'
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER2] = LAYOUT_reviung34(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_F11,  KC_F12,  KC_WH_L, KC_WH_R, XXXXXXX,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX,
+    KC_F11,  KC_F12,  KC_BTN2, KC_BTN3, KC_BTN1,       XXXXXXX, XXXXXXX, KC_COMM, KC_DOT,  XXXXXXX,
                                KC_ADJ,  LO2_SFT,       _______, KC_ENT
   ),
 
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,----------------------------------.  ,----------------------------------.
 | Reset| VAL+ | SAT+ | HUE+ | MODE+|  |      |      |      |Print | Ins  |
 +------+------+------+------+------|  |------+------+------+------+------|
-|      | BtnR | BtnM | BtnL |      |  |      |      |      |ScLock|Pause |
+|      |      |      |      |      |  |      |      |      |ScLock|Pause |
 |------+------+------+------+------|  |------+------+------+------+------|
 |RGB ON| VAL- | SAT- | HUE- | MODE-|  |      |      |      |      |      |
 `------+------+------+------+------|  |------+------+------+------+------'
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] = LAYOUT_reviung34(
     RESET,   RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
-    XXXXXXX, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,
     RGB_TOG, RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                _______,  _______,      _______, KC_ENT
   ),
