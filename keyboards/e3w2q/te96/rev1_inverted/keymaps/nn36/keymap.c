7656d0bce49f17044c851f5f39ae34f68bc3c8d4
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-01-16 13:13:01 +0900

    te96: modify keymaps layout as actual one.

--- keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
+++ keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
@@ -47,117 +47,94 @@ enum layer_names {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  /* Default Layer
-   *  Master Side (USB Cable Connected Side)
-   *  ,-------------------------------------------
-   *  | XXXX XXXX Ctrl/Esc Q    W    E    R     T
-   *  | XXXX XXXX Shft/Tab A    S    D    F     G
-   *  | XXXX XXXX XXXX     Z    X    C    V     B
-   *  | XXXX XXXX XXXX  XXXX XXXX XXXX XXXX  XXXX
-   *  | XXXX XXXX XXXX  XXXX XXXX XXXX MO(1) Spce
-   *  | XXXX XXXX XXXX  XXXX XXXX XXXX XXXX  XXXX
-   *  `-------------------------------------------
-   *  Slave Side (USB Cable Not Connected Side)
-   *  ,---------------------------------------------
-   *  |    Y    U     I    O    P GUI/Del XXXX XXXX
-   *  |    H    J     K    L    - Alt/BS  XXXX XXXX
-   *  |    N    M     ,    .    / XXXX    XXXX XXXX
-   *  | XXXX XXXX  XXXX XXXX XXXX XXXX    XXXX XXXX
-   *  | Entr MO(2) XXXX XXXX XXXX XXXX    XXXX XXXX
-   *  | XXXX XXXX  XXXX XXXX XXXX XXXX    XXXX XXXX
-   *  `---------------------------------------------
-   *  ,-----------------------------------------
-   *  |   XX   XX   XX   E6   D6   C6   B6   A6
-   *  |   XX   XX   XX   E5   D5   C5   B5   A5
-   *  |   XX   XX   XX   E4   D4   C4   B4   A4
-   *  |   XX   XX   F3   XX   XX   XX   B3   A3
-   *  |   XX   XX   XX   XX   XX   XX   XX   XX
-   *  |   XX   XX   XX   XX   XX   XX   XX   XX
-   *  `-----------------------------------------
+  /* BASE
+,----------------------------------.  ,----------------------------------.
+|   Q  |   W  |   E  |   R  |   T  |  |   Y  |   U  |   I  |   O  |   P  |
+|------+------+------+------+------|  |------+------+------+------+------|
+| A Sft| S Ctl| D Win| F Alt|   G  |  |   H  | J Alt| K Win| L Ctl| ; Sft|
+|------+------+------+------+------|  |------+------+------+------+------|
+|   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  |   ,  |   .  |   /  |
+`------+------+------+------+------|  |------+------+------+------+------'
+              |      |LowSpc|Lo2Sft|  |  BS  |RaiEnt|      |
+              `--------------------'  `--------------------'
    */
-
-  [_BASE] = LAYOUT( /* Base */
-    XXXXXXX,  XXXXXXX,  XXXXXXX, KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,
-    XXXXXXX,  XXXXXXX,  XXXXXXX, SF_A,     CT_S,     GU_D,     AL_F,     KC_G,
-    XXXXXXX,  XXXXXXX,  XXXXXXX, KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,
-    XXXXXXX,  XXXXXXX,  LO2_SFT, XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO  ,  LOW_SPC,
-    XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX, XXXXXXX,  XXXXXXX,
-    KC_H,     AL_J,     GU_K,     CT_L,     SF_SC,    XXXXXXX, XXXXXXX,  XXXXXXX,
-    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX, XXXXXXX,  XXXXXXX,
-    RAI_ENT,  KC_NO  ,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_BSPC, XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX
+  [_BASE] = LAYOUT_nn36(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    SF_A,    CT_S,    GU_D,    AL_F,    KC_G,          KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                      XXXXXXX, LOW_SPC, LO2_SFT,       KC_BSPC, RAI_ENT, XXXXXXX
   ),
 
-  /* Lower Layer */
-  [_LOWER] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_ESC,   KC_TILD,  KC_LCBR,  KC_RCBR,  KC_NO  ,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO,    KC_GRV,   KC_LBRC,  KC_RBRC,  KC_NO  ,
-    XXXXXXX,  XXXXXXX,  KC_ADJ ,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO  ,  LOW_SPC,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  KC_UNDS,  KC_PLUS,  KC_DQT,   KC_PIPE,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  KC_MINS,  KC_EQL,   KC_QUOT,  KC_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_ENT ,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  /* LOWER
+,----------------------------------.  ,----------------------------------.
+|   !  |   @  |   #  |   $  |   %  |  |   ^  |   &  |   *  |   (  |   )  |
+|------+------+------+------+------|  |------+------+------+------+------|
+|  ESC |   ~  |   {  |   }  |      |  |      |   _  |   +  |   "  |   |  |
+|------+------+------+------+------|  |------+------+------+------+------|
+|      |   `  |   [  |   ]  |      |  |      |   -  |   =  |   '  |   \  |
+`------+------+------+------+------|  |------+------+------+------+------'
+              |      |LowSpc|Adjust|  |  BS  | Enter|      |
+              `--------------------'  `--------------------'
+   */
+  [_LOWER] = LAYOUT_nn36(
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_ESC,  KC_TILD, KC_LCBR, KC_RCBR, XXXXXXX,       XXXXXXX, KC_UNDS, KC_PLUS, KC_DQT,  KC_PIPE,
+    XXXXXXX, KC_GRV,  KC_LBRC, KC_RBRC, XXXXXXX,       XXXXXXX, KC_MINS, KC_EQL,  KC_QUOT, KC_BSLS,
+                      XXXXXXX, LOW_SPC, KC_ADJ,        _______, KC_ENT , XXXXXXX
   ),
 
-  /* Lower2 Layer */
-  [_LOWER2] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_F11,   KC_F12,   XXXXXXX,  KC_PLUS,  KC_MINS,
-    XXXXXXX,  XXXXXXX,  LO2_SFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO  ,  KC_ADJ ,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_ASTR,  KC_SLSH,  KC_COMM,  KC_DOT,   KC_EQL,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_ENT,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  /* LOWER2
+,----------------------------------.  ,----------------------------------.
+|   1  |   2  |   3  |   4  |   5  |  |   6  |   7  |   8  |   9  |   0  |
+|------+------+------+------+------|  |------+------+------+------+------|
+|  F1  |  F2  |  F3  |  F4  |  F5  |  |  F6  |  F7  |  F8  |  F9  |  F10 |
+|------+------+------+------+------|  |------+------+------+------+------|
+|  F11 |  F12 |      |   +  |   -  |  |   *  |   /  |   ,  |   .  |   =  |
+`------+------+------+------+------|  |------+------+------+------+------'
+              |      |Adjust|Lo2Sft|  |  BS  | Enter|      |
+              `--------------------'  `--------------------'
+   */
+  [_LOWER2] = LAYOUT_nn36(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_F11,  KC_F12,  XXXXXXX, KC_PLUS, KC_MINS,       KC_ASTR, KC_SLSH, KC_COMM, KC_DOT,  KC_EQL,
+                      XXXXXXX, KC_ADJ,  LO2_SFT,       _______, KC_ENT , XXXXXXX
   ),
 
-  /* Raise Layer */
-  [_RAISE] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  CG_LEFT,  CG_RGHT,  SG_LEFT,  SG_RGHT,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_LANG2,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_WH_L,  KC_WH_D,  KC_WH_U,  KC_WH_R,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  KC_NO  ,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO  ,  KC_SPC ,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    XXXXXXX,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_LANG1, KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_APP,   KC_BSPC, S(KC_TAB), KC_TAB,   KC_DEL,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-    RAI_ENT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,
-    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  /* RAISE
+,----------------------------------.  ,----------------------------------.
+|CtWn<-|CtWn->|SfWn<-|SfWn->|      |  |      | Home |PageDn|PageUp| End  |
+|------+------+------+------+------|  |------+------+------+------+------|
+| Shift| Ctrl |  Gui |  Alt |ImeOff|  |ImeOn | Left | Down | Up   | Right|
+|------+------+------+------+------|  |------+------+------+------+------|
+| Wh_L | Wh_D | Wh_U | Wh_R |      |  |  App |  BS  |SftTab| Tab  | Del  |
+`------+------+------+------+------|  |------+------+------+------+------'
+              |      | Space|      |  |  BS  |RaiEnt|      |
+              `--------------------'  `--------------------'
+   */
+  [_RAISE] = LAYOUT_nn36(
+    CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_LANG2,      KC_LANG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,       KC_APP,  KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
+                      XXXXXXX, KC_SPC,  XXXXXXX,       _______, RAI_ENT, XXXXXXX
   ),
 
-  [_ADJUST] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NO  ,  _______,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSCR,  KC_INS ,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_SLCK,  KC_PAUS,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_ENT,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  /* ADJUST
+,----------------------------------.  ,----------------------------------.
+| Reset| VAL+ | SAT+ | HUE+ | MODE+|  |      |      |      |Print | Ins  |
++------+------+------+------+------|  |------+------+------+------+------|
+|      |      |      |      |      |  |      |      |      |ScLock|Pause |
+|------+------+------+------+------|  |------+------+------+------+------|
+|RGB ON| VAL- | SAT- | HUE- | MODE-|  |      |      |      |      |      |
+`------+------+------+------+------|  |------+------+------+------+------'
+              |      |LowSpc|Lo2Sft|  |  BS  | Enter|      |
+              `--------------------'  `--------------------'
+   */
+  [_ADJUST] = LAYOUT_nn36(
+    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                      XXXXXXX, _______,  _______,      _______, KC_ENT , XXXXXXX
   ),
 };
 
