9db47556ea9ec4687e012a9e0e9a43e7d352e261
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-05-29 17:35:58 +0900

    crkbd_m: new with square matrix

---
+++ keyboards/crkbd_m/keymaps/default/config.h
@@ -0,0 +1,65 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// #ifndef CONFIG_USER_H
+// #define CONFIG_USER_H
+#pragma once
+
+// place overrides here
+// #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+// #define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+
+#undef TAPPING_TERM
+// #define TAPPING_TERM 200
+#define TAPPING_TERM 160
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+// #undef TAPPING_FORCE_HOLD
+#define TAPPING_FORCE_HOLD
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+// 押しっぱなしの時の反応までのインターバル
+#undef  MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 16
+// カーソルの移動スピード
+#undef  MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 2
+// マウス操作がトップスピードになるまでの時間
+#undef  MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
+
+// If you need more program area, try select and reduce rgblight modes to use.
+
+// Selection of RGBLIGHT MODE to use.
+#if defined(LED_ANIMATIONS)
+   #define RGBLIGHT_EFFECT_BREATHING
+   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+   #define RGBLIGHT_EFFECT_SNAKE
+   #define RGBLIGHT_EFFECT_KNIGHT
+   #define RGBLIGHT_EFFECT_CHRISTMAS
+   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   //#define RGBLIGHT_EFFECT_RGB_TEST
+   //#define RGBLIGHT_EFFECT_ALTERNATING
+#endif
+
+// #endif /* CONFIG_USER_H */
