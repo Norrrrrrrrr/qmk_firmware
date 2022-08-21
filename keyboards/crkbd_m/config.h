dd09a2d3f719abc10e7a33b6b7efcaaecc3dcb72
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-08-21 12:30:37 +0900

    crkbd, nirvana: Move keyboard USB IDs and strings, Remove useless defines

--- keyboards/crkbd_m/config.h
+++ keyboards/crkbd_m/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x4653
-#define PRODUCT_ID   0x0001
-#define DEVICE_VER   0x0001
-#define MANUFACTURER Norrrrrrrrr
-#define PRODUCT      crkbd_m
-
 /* key matrix size */
 #define MATRIX_ROWS (5 * 2)
 #define MATRIX_COLS 5
@@ -146,10 +139,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LAYER_STATE_8BIT
 //#define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
-
-/* disable these deprecated features by default */
-// #define NO_ACTION_MACRO
-// #define NO_ACTION_FUNCTION
 
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
