dc0065973f164229c67b10092684ab5a14ffe809
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-08-31 21:56:05 +0900

    remove depricated define

--- keyboards/nirvana_nn/config.h
+++ keyboards/nirvana_nn/config.h
@@ -35,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define MATRIX_ROW_PINS { F6, F7, B1, B3 }
 #define MATRIX_COL_PINS { F4, D4, C6, D7, E6, B4 }
-#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
