1baa5ff8a30f65cf4e3a4fc17f6b96d683ae57c4
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-11-13 12:52:55 +0900

    te96: remove unconnected pins

--- keyboards/e3w2q/te96/rev1_inverted/config.h
+++ keyboards/e3w2q/te96/rev1_inverted/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 6*2 //master: 6 rows, slave: 6 rows
-#define MATRIX_COLS 8
+#define MATRIX_COLS 6
 
 /*
  * Keyboard Matrix Assignments
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
