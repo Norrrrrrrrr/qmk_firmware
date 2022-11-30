a7c50e6f30d81ae773596ac87cd5ef02309c9f91
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-12-01 03:39:43 +0900

    nirvana: reduce code size

--- keyboards/nirvana_nn/config.h
+++ keyboards/nirvana_nn/config.h
@@ -133,8 +133,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* disable action features */
 //#define NO_ACTION_LAYER
+#define LAYER_STATE_8BIT
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NO_ACTION_ONESHOT
 
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
