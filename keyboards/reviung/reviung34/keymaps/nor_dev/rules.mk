e047a9f239aa2aa6377e4fa488765f0c835b58c7
Norrrrrrrrr <nor.fenstrat@gmail.com>
2022-12-10 10:26:09 +0900

    all: disable LOCKING_XXX and EXTRAKEY, command for all keyboard

--- keyboards/reviung/reviung34/keymaps/nor_dev/rules.mk
+++ keyboards/reviung/reviung34/keymaps/nor_dev/rules.mk
@@ -3,7 +3,7 @@ RGBLIGHT_ENABLE = yes
 #s nn_rev01
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 # CONSOLE_ENABLE = yes         # Output DEBUG_MATRIX_SCAN_RATE
 COMMAND_ENABLE = no        # Commands for debug and configuration
