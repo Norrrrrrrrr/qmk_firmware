049ee1ebb8deb0e3adcd128b4dfdcef2f8853e4f
Norrrrrrrrr <nor.fenstrat@gmail.com>
2023-06-03 20:13:04 +0900

    crkbd_m, nirvana: delete comment outs

--- keyboards/nirvana_nn/keymaps/default/config.h
+++ keyboards/nirvana_nn/keymaps/default/config.h
@@ -26,9 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 #define EE_HANDS
 
-// #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
-// #define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
-
 #undef TAPPING_TERM
 // #define TAPPING_TERM 200
 #define TAPPING_TERM 160
