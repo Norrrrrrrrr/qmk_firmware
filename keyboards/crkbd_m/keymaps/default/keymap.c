a29c4b93dea51a41ecd3bec8c37cf7d8f98a18e1
Norrrrrrrrr <nor.fenstrat@gmail.com>
2023-11-25 18:29:20 +0900

    nirvana,reviung: Add combo keys

--- keyboards/crkbd_m/keymaps/default/keymap.c
+++ keyboards/crkbd_m/keymaps/default/keymap.c
@@ -92,7 +92,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
                      |      |LowSpc|Lo2Sft|  |  BS  |RaiEnt|      |
                      `--------------------'  `--------------------'
-    Combo: F&G = ImeOff, H&J = ImeOn, N&M = App, M&"," = "-", X&C = CLK_R, C&V = CLK_L
    */
   [_BASE] = LAYOUT_split_3x5_sqmtrx(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
