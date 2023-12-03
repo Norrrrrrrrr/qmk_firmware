53b3b66719b9ceae007c4e61d21e78f7114e2c84
Norrrrrrrrr <nor.fenstrat@gmail.com>
2023-12-03 12:47:05 +0900

    all: remove useless includes

--- keyboards/nirvana_nn/keymaps/default/keymap.c
+++ keyboards/nirvana_nn/keymaps/default/keymap.c
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "keycode.h"
-#include "quantum_keycodes.h"
 
 enum layer_names {
     _BASE = 0,
