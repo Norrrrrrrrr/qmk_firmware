5930d0a7c90a7df9e39cb278b94565f9e1b19708
Norrrrrrrrr <nor.fenstrat@gmail.com>
2024-09-14 18:47:51 +0900

    nn36: fix position of space

--- keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
+++ keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_ESC,  KC_TILD, KC_LCBR, KC_RCBR, XXXXXXX,       XXXXXXX, KC_UNDS, KC_PLUS, KC_DQT,  KC_PIPE,
     XXXXXXX, KC_GRV,  KC_LBRC, KC_RBRC, XXXXXXX,       XXXXXXX, KC_MINS, KC_EQL,  KC_QUOT, KC_BSLS,
-                      XXXXXXX, LOW_SPC, KC_ADJ,        _______, KC_ENT , XXXXXXX
+                      XXXXXXX, LOW_SPC, KC_ADJ,        _______, KC_ENT,  XXXXXXX
   ),
 
   /* LOWER2
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F12,  KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F11,  XXXXXXX, KC_COMM, KC_DOT,  KC_EQL ,
-                      XXXXXXX, KC_ADJ,  LO2_SFT,       _______, KC_ENT , XXXXXXX
+                      XXXXXXX, KC_ADJ,  LO2_SFT,       _______, KC_ENT,  XXXXXXX
   ),
 
   /* RAISE
@@ -154,7 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SCRL, KC_PAUS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      XXXXXXX, _______,  _______,      _______, KC_ENT , XXXXXXX
+                      XXXXXXX, _______,  _______,      _______, KC_ENT,  XXXXXXX
   ),
 };
 
