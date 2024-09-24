5f2c304525f352521494eff0d84a8f830d12cb22
Norrrrrrrrr <nor.fenstrat@gmail.com>
2024-09-24 18:05:59 +0900

    nn36: move Lo2 to RH

--- keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
+++ keyboards/e3w2q/te96/rev1_inverted/keymaps/nn36/keymap.c
@@ -40,7 +40,7 @@ enum layer_names {
 
 #define LOW_SPC LT(_LOWER, KC_SPC)
 #define RAI_ENT LT(_RAISE, KC_ENT)
-#define LO2_SFT LT(_LOWER2, KC_LSFT)
+#define LO2_BS LT(_LOWER2, KC_BSPC)
 #define KC_ADJ MO(_ADJUST)
 
 /* Combo */
@@ -75,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |   Z  |   X  |   C  |   V  |   B  |                |   N  |   M  |   ,  |   .  |   /  |
 |    ^WH_U^^CLK_R^^CLK_L^ ^WH_D^   |                |    ^App^  ^ - ^    |      |      |
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
-                     |      |LowSpc|Lo2Sft|  |  BS  |RaiEnt|      |
+                     |      |LowSpc| Shift|  | Lo2BS|RaiEnt|      |
                      `--------------------'  `--------------------'
    */
   [_BASE] = LAYOUT_nn36(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     SF_A,    CT_S,    GU_D,    AL_F,    KC_G,          KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                      XXXXXXX, LOW_SPC, LO2_SFT,       KC_BSPC, RAI_ENT, XXXXXXX
+                      XXXXXXX, LOW_SPC, KC_LSFT,       LO2_BS, RAI_ENT, XXXXXXX
   ),
 
   /* LOWER
@@ -93,14 +93,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |      |   `  |   [  |   ]  |      |                |      |   -  |   =  |   '  |   \  |
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
-                     |      |LowSpc|Adjust|  |  BS  | Enter|      |
+                     |      |LowSpc| Shift|  |Adjust| Enter|      |
                      `--------------------'  `--------------------'
    */
   [_LOWER] = LAYOUT_nn36(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_ESC,  KC_TILD, KC_LCBR, KC_RCBR, XXXXXXX,       XXXXXXX, KC_UNDS, KC_PLUS, KC_DQT,  KC_PIPE,
     XXXXXXX, KC_GRV,  KC_LBRC, KC_RBRC, XXXXXXX,       XXXXXXX, KC_MINS, KC_EQL,  KC_QUOT, KC_BSLS,
-                      XXXXXXX, LOW_SPC, KC_ADJ,        _______, KC_ENT,  XXXXXXX
+                      XXXXXXX, LOW_SPC, _______,        KC_ADJ,  KC_ENT,  XXXXXXX
   ),
 
   /* LOWER2
@@ -111,14 +111,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F11 |      |   ,  |   .  |   =  |
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
-                     |      |Adjust|Lo2Sft|  |  BS  | Enter|      |
+                     |      |Adjust| Shift|  | Lo2BS| Enter|      |
                      `--------------------'  `--------------------'
    */
   [_LOWER2] = LAYOUT_nn36(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F12,  KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F11,  XXXXXXX, KC_COMM, KC_DOT,  KC_EQL ,
-                      XXXXXXX, KC_ADJ,  LO2_SFT,       _______, KC_ENT,  XXXXXXX
+                      XXXXXXX, KC_ADJ,  _______,       LO2_BS,  KC_ENT,  XXXXXXX
   ),
 
   /* RAISE
@@ -129,14 +129,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |      |      |      |      |      |                |      |  BS  |SftTab| Tab  | Del  |
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
-                     |      | Space|      |  |  BS  |RaiEnt|      |
+                     |      | Space| Shift|  |  BS  |RaiEnt|      |
                      `--------------------'  `--------------------'
    */
   [_RAISE] = LAYOUT_nn36(
     CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX,       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
-                      XXXXXXX, KC_SPC,  XXXXXXX,       _______, RAI_ENT, XXXXXXX
+                      XXXXXXX, KC_SPC,  _______,       KC_BSPC,  RAI_ENT, XXXXXXX
   ),
 
   /* ADJUST
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |      |      |      |      |      |                |      |      |      |      |      |
 `------+------+------+------+------+------.  ,------+------+------+------+------+------'
-                     |      |LowSpc|Lo2Sft|  |  BS  | Enter|      |
+                     |      |LowSpc| Shift|  | Lo2BS| Enter|      |
                      `--------------------'  `--------------------'
    */
   [_ADJUST] = LAYOUT_nn36(
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LOW_SPC:
-        case LO2_SFT:
+        case LO2_BS:
         case RAI_ENT:
             return 0;
             break;
@@ -178,7 +178,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         return 400;
     /* Typing layer quickly */
     case LOW_SPC:
-    case LO2_SFT:
+    case LO2_BS:
     case RAI_ENT:
         return 130;
     default:
