c27cd9478e851c342b014de4eb75f1559a9305b6
Norrrrrrrrr <nor.fenstrat@gmail.com>
2024-11-08 18:27:37 +0900

    all: same key layout

--- keyboards/nirvana_nn/keymaps/default/keymap.c
+++ keyboards/nirvana_nn/keymaps/default/keymap.c
@@ -16,10 +16,10 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE = 0,
-    _LOWER,
-    _RAISE,
-    _ADJUST
+  _BASE = 0,
+  _LOWER,
+  _RAISE,
+  _ADJUST
 };
 
 enum my_keycodes {
@@ -83,14 +83,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |   Z  |   X  |   C  |   V  |   B  |                |   N  |   M  |   ,  |   .  |   /  |
 |    ^WH_U^^CLK_R^^CLK_L^ ^WH_D^   |                |    ^App^  ^ - ^    |      |      |
 `---------------------------+------+------.  ,------+------+---------------------------'
-                     |      |LowSpc|  BS  |  | Shift|RaiEnt|      |
-                     `--------------------'  `--------------------'
+                            |LowSpc|  BS  |  | Shift|RaiEnt|
+                            `-------------'  `-------------'
    */
   [_BASE] = LAYOUT(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     SF_A,    CT_S,    GU_D,    AL_F,    KC_G,          KC_H,    AL_J,    GU_K,    CT_L,    SF_SC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                      XXXXXXX, LOW_SPC, KC_BSPC,       KC_LSFT, RAI_ENT, XXXXXXX
+                               LOW_SPC, KC_BSPC,       KC_LSFT, RAI_ENT
   ),
 
   /* LOWER
@@ -101,14 +101,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |  F1  |  F2  |  F3  |  F4  |  F5  |                |  F6  |  F7  |  F8  |  F9  |  F10 |
 `---------------------------+------+------.  ,------+------+---------------------------'
-                     |      |LowSpc|  BS  |  | Shift|Adjust|      |
-                     `--------------------'  `--------------------'
+                            |LowSpc|  BS  |  | Shift|Adjust|
+                            `-------------'  `-------------'
    */
   [_LOWER] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_ESC,  KC_GRV,  KC_BSLS, KC_QUOT, KC_F11,        KC_F12,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-                      XXXXXXX, LOW_SPC, _______,       _______, KC_ADJ,  XXXXXXX
+                               LOW_SPC, _______,       _______, KC_ADJ
   ),
 
   /* RAISE
@@ -119,14 +119,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |      |      |      |      |      |                |      |  BS  |SftTab| Tab  | Del  |
 `---------------------------+------+------.  ,------+------+---------------------------'
-                     |      | Space|  BS  |  | Shift|RaiEnt|      |
-                     `--------------------'  `--------------------'
+                            | Space|  BS  |  | Shift|RaiEnt|
+                            `-------------'  `-------------'
    */
   [_RAISE] = LAYOUT(
     CG_LEFT, CG_RGHT, SG_LEFT, SG_RGHT, XXXXXXX,       XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX,       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_BSPC,S(KC_TAB),KC_TAB,  KC_DEL,
-                      XXXXXXX, KC_SPC,  _______,       _______, RAI_ENT, XXXXXXX
+                               KC_SPC,  _______,       _______, RAI_ENT
   ),
 
   /* ADJUST
@@ -137,14 +137,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------|                |------+------+------+------+------|
 |      |      |      |      |      |                |      |      |      |      |      |
 `---------------------------+------+------.  ,------+------+---------------------------'
-                     |      |LowSpc|  BS  |  | Shift|RaiEnt|      |
-                     `--------------------'  `--------------------'
+                            |LowSpc|  BS  |  | Shift|RaiEnt|
+                            `-------------'  `-------------'
    */
   [_ADJUST] = LAYOUT(
     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_INS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, KC_SCRL, KC_PAUS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      XXXXXXX, _______, _______,       _______, _______, XXXXXXX
+                               _______, _______,       _______, _______
   ),
 };
 
